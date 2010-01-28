5344e1ac11d7bab81a29a13a13b9ca6835b0760e
Pavel Vyazankin <pavel.vyazankin@gmail.com>
2010-01-28 23:02:16 +0300

    command line options, readme

--- src/main/java/dart/blackcat/messj/Boot.java
+++ src/main/java/dart/blackcat/messj/Boot.java
@@ -1,25 +1,39 @@
 package dart.blackcat.messj;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 public class Boot {
 	
-	private static final Log LOG = LogFactory.getLog(Boot.class);
+//	private static final Log LOG = LogFactory.getLog(Boot.class);
 
 	/**
-	 * messj
-	 * megaprog
 	 * @param args
 	 */
 	public static void main(String[] args) {
 
-		String login = args[0];
-		String passw = args[1];
-		String host = args[2];
-		int port = Integer.parseInt(args[3]);
-		
-		/*MessjAgent agent = */new MessjAgent(login, passw, host, port);
+		try {
+			String login = args[0];
+			String passw = args[1];
+			String host = args[2];
+			int port = Integer.parseInt(args[3]);
+			String user = args[4];
+			
+			if (
+					login == null || login.isEmpty() || 
+					passw == null || passw.isEmpty() || 
+					host == null  || host.isEmpty()  || 
+					port <= 0  || 
+					user == null  || user.isEmpty()  
+			) {
+				showErrorAndExit();
+			}
+			/*MessjAgent agent = */new MessjAgent(login, passw, host, port);
+		} catch (Exception e) {
+			showErrorAndExit();
+		}
+	}
+	
+	protected static void showErrorAndExit() {
+		System.out.println("messj <login> <password> <host> <port> <authorized_user>");
 	}
 
 }
